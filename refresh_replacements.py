5451620dec7e7fe080ca660df18ccba3e95f1ed3
pajowu <github@ca.pajowu.de>
2015-06-07 01:36:09 +0200

    added script to crate "dict.json" from couch db, updated proxy.py, word substitution now partly works

---
+++ refresh_replacements.py
@@ -0,0 +1,12 @@
+import requests
+import json
+req = requests.get("http://couchdb.pajowu.de/neulandeuphonie/_design/api/_view/replacements_by_word")
+data = req.json()
+rep = {}
+for i in data['rows']:
+    rep[i['key']] = []
+    for value in i['value']:
+        rep[i['key']].append(value)
+with open("dict.json","w") as dictfile:
+    json.dump(rep,dictfile,indent=4)
+#import pdb;pdb.set_trace()
