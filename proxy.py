3a3f854974ef1e08afb69a7fdb2d79024b193ea1
Johnathan316 <martin-heckel@web.de>
2015-06-06 22:01:32 +0200

    Wortersetzung funktioniert, JSON wird generiert

--- proxy.py
+++ proxy.py
@@ -4,7 +4,6 @@ import sys; reload(sys);sys.setdefaultencoding("utf-8")
 """
 This example builds on mitmproxy's base proxying infrastructure to
 implement functionality similar to the "sticky cookies" option.
-
 Heads Up: In the majority of cases, you want to use inline scripts.
 """
 import os
@@ -24,6 +23,7 @@ for key,value in liste.items():
     expression.append((re.compile((key), re.IGNORECASE), value))
     i=i+1
 
+
 class CensorMaster(controller.Master):
     def run(self):
         try:
@@ -32,22 +32,31 @@ class CensorMaster(controller.Master):
             self.shutdown()
 
     def handle_response(self, flow):
+        #import pdb;pdb.set_trace()
+        print(flow.request.headers)
         try:
+            stat = {"type":"statistic", "changes":[]}
+            stat['url'] = flow.request.url
             attrs = dict((x.lower(),y) for x, y in flow.response.headers)
             if 'content-type' in attrs:
-                print "content type"
                 if (attrs ['content-type'] == 'text/html'):
-                    print "html/css"
                     #flow.response.content += ("<style>" + stylesheet + "</style>")
                     tmp = flow.response.content.split("</head>") 
                     tmp[0]+="<style>" + stylesheet + "</style>" + "</head>"
                     flow.response.content = "".join(tmp)
         
                 for key,value in expression:
+                    change = {"word":"", "replaced_by":"", "count":"1"}
                     value_rand = random.choice(value)
-                    flow.response.content = re.sub(key,value_rand,flow.response.content)
+                    subn_res = re.subn(key,value_rand,flow.response.content)
+                    flow.response.content = subn_res[0]
                     #import pdb; pdb.set_trace()
                     flow.reply()
+                    change['word'] = str(key.pattern)
+                    change['replaced_by'] = (str(value_rand))
+                    change['count'] = str(subn_res[1])
+                    stat['changes'].append(change)
+                print str(json.dumps(stat))
         
         except UnicodeDecodeError, e:
             print (flow.response.content)
