80def317bbe310ca0e9440759e2df155396977ce
Markus Schmidl <markus.schmidl@ymail.com>
2015-06-15 20:35:14 +0200

    crude d3 visualization implemented

--- statistik/js/stats.js
+++ statistik/js/stats.js
@@ -1,11 +1,9 @@
 var waiting = false;
 function refreshStats() {
 	$.getJSON("http://couchdb.pajowu.de/neulandeuphonie/_design/api/_view/count_host_word_replacements?group_level=1", function(data){
-		var table = $("table.wortUrl");
-		table.empty();
+		//var table = $("table.wortUrl");
+		//table.empty();
 
-		var tableHost = $("table.hostName");
-		tableHost.empty();
 		$.each(data.rows, function(index, entry){
 	
 			//split key into parts seperated at points
@@ -13,31 +11,25 @@ function refreshStats() {
 			var hostname = hn[1] + "." + hn[0];
 
 			//check if hostname is a ip
-			if (!/[0-9]/.test(hn[0].charAt(0))) {
-				entry.key[0] = hostname;
-			}
+			if (!/[0-9]/.test(hn[0].charAt(0))) entry.key[0] = hostname;
 		})
 
 		//make new table with all same keys summarized
 		var newObj = {};
 		for(i in data.rows){
  			var item = data.rows[i];
-    			if(newObj[item.key[0]] === undefined){
-        			newObj[item.key[0]] = 0;
-    			}
-   			 newObj[item.key[0]] += item.value;
+    			if(newObj[item.key[0]] === undefined) newObj[item.key[0]] = 0;
+   			newObj[item.key[0]] += item.value;
 		}
 
 		var result = {};
 		result.rows = [];
-		for(i in newObj){
-   			result.rows.push({'key':i,'value':newObj[i]});
-		}
+		for(i in newObj) result.rows.push({'key':i,'value':newObj[i]});
 
 
 		//sort table
 		result.rows.sort(function(a,b){return b.value-a.value})
-
+/*
 		//draw table
 		var valueCount = 0;
 		$.each(result.rows, function(index, entry){
@@ -48,7 +40,67 @@ function refreshStats() {
 
 		var row = $("<tr><td></td><td><hr/></td></tr>		<tr><td>Gesamt</td><td>"+valueCount+"</td></tr>");
 		table.append(row);
-	})
+*/
+		//make new data for d3 chart
+		var chart = {
+			     "name": "hostname",
+			     "children": [
+			     ]};
+		for (i in newObj) {
+			chart.children.push({'name':i,'size':newObj[i]});
+		}
+
+		console.log(chart);
+
+		//draw d3 chart
+		var diameter = 960,
+		format = d3.format(",d"),
+    		color = d3.scale.category20c();
+
+		var bubble = d3.layout.pack()
+    			.sort(null)
+    			.size([diameter, diameter])
+			.padding(1.5);
+
+		var svg = d3.select("div.wortUrl").append("svg")
+			.attr("width", diameter)
+			.attr("height", diameter)
+			.attr("class", "bubble");
+
+		var node = svg.selectAll(".node")
+			.data(bubble.nodes(classes(chart))
+			.filter(function(d) { return !d.children; }))
+			.enter().append("g")
+			.attr("class", "node")
+			.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		node.append("title")
+			.text(function(d) { return d.className + ": " + format(d.value); });
+
+		node.append("circle")
+			.attr("r", function(d) { return d.r; })
+			.style("fill", function(d) { return color(d.packageName); });
+
+		node.append("text")
+			.attr("dy", ".5em")
+			.style("text-anchor", "middle")
+			.text(function(d) { return d.className.substring(0, d.r / 3); });
+
+		// Returns a flattened hierarchy containing all leaf nodes under the root.
+		function classes(chart) {
+			var classes = [];
+
+			function recurse(name, node) {
+				if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+				else classes.push({packageName: name, className: node.name, value: node.size});
+			}
+
+			recurse(null, chart);
+			return {children: classes};
+		}
+
+		d3.select(self.frameElement).style("height", diameter + "px");
+		})
 
 	$.getJSON( "http://couchdb.pajowu.de/neulandeuphonie/_design/api/_view/count_word_replacements?group_level=1" , function(data){ 
 		console.log(data);
@@ -64,7 +116,7 @@ function refreshStats() {
 		var row = $("<tr><td></td><td><hr/></td></tr>		<tr><td>Gesamt</td><td>"+valueCount+"</td></tr>");
 		table.append(row);
 	})
-};
+};/*
 //check for browser support
 if(typeof(EventSource)!=="undefined") {
 	//create an object, passing it the name and location of the server side script
@@ -81,6 +133,6 @@ if(typeof(EventSource)!=="undefined") {
 }
 else {
 	document.getElementById("serverData").innerHTML="Whoops! Your browser doesn't receive server-sent events.";
-}
+}*/
 
 refreshStats();
