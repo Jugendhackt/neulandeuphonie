8104c1d294cbc53dabe27203f6a9d3e96122db7e
pajowu <github@ca.pajowu.de>
2015-06-16 15:57:35 +0200

    added jQuery and bootstrap

--- statistik/js/stats.js
+++ statistik/js/stats.js
@@ -29,18 +29,6 @@ function refreshStats() {
 
 		//sort table
 		result.rows.sort(function(a,b){return b.value-a.value})
-/*
-		//draw table
-		var valueCount = 0;
-		$.each(result.rows, function(index, entry){
-			valueCount += entry.value
-			var row = $("<tr><td>"+entry.key+"</td><td>"+entry.value+"</td></tr>");
-			table.append(row);
-		})
-
-		var row = $("<tr><td></td><td><hr/></td></tr>		<tr><td>Gesamt</td><td>"+valueCount+"</td></tr>");
-		table.append(row);
-*/
 		//make new data for d3 chart
 		var chart = {
 			     "name": "hostname",
@@ -108,7 +96,7 @@ function refreshStats() {
 		})
 
 	$.getJSON( "http://couchdb.pajowu.de/neulandeuphonie/_design/api/_view/count_word_replacements?group_level=1" , function(data){ 
-		console.log(data);
+		
 		var table = $("table.wortAnzahl");
 		table.empty();
 		var valueCount = 0;
@@ -121,23 +109,6 @@ function refreshStats() {
 		var row = $("<tr><td></td><td><hr/></td></tr>		<tr><td>Gesamt</td><td>"+valueCount+"</td></tr>");
 		table.append(row);
 	})
-};/*
-//check for browser support
-if(typeof(EventSource)!=="undefined") {
-	//create an object, passing it the name and location of the server side script
-	var eSource = new EventSource("http://couchdb.pajowu.de/neulandeuphonie/_changes?filter=api/statistic&feed=eventsource");
-	//detect message receipt
-	eSource.onmessage = function(event) {
-		//write the received data to the page
-		console.log("got sse");
-		if (!waiting) {
-			waiting = true
-			setInterval(function(){waiting=false;refreshStats()},1000);
-		}
-	};
 }
-else {
-	document.getElementById("serverData").innerHTML="Whoops! Your browser doesn't receive server-sent events.";
-}*/
 
 refreshStats();
