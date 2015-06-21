14e46530c7af8a1af79ef6b67936830909fe74ca
Markus Schmidl <markus.schmidl@ymail.com>
2015-06-22 01:42:11 +0200

    cleaned up code

--- statistik/js/main.js
+++ statistik/js/main.js
@@ -1,98 +1,108 @@
+function sort(data) {
+	//make all same keys summarized
+	var diff = 0;
+	if (/\[/.test(data.rows[0].key)) diff = 1;
+	var newObj = {};
+	for(i in data.rows){
+		var item = data.rows[i];
+		var item2;
+		if (diff) item2 = item.key[0];
+		else item2 = item.key;
+		if(newObj[item2] === undefined) newObj[item2] = 0;
+		newObj[item2] += item.value;
+	}
+	var result = {};
+	result.rows = [];
+	for(i in newObj) result.rows.push({'key':i,'value':newObj[i]});
+
+	//sort
+	result.rows.sort(function(a,b){return b.value-a.value})
+	return [result, newObj];
+}
+
 function drawChart(struct, data) {
-		
-		//make new table with all same keys summarized
-		var diff = 0;
-		if (/\[/.test(data.rows[0].key)) diff = 1;
-		var newObj = {};
-		for(i in data.rows){
- 			var item = data.rows[i];
-			var item2;
-			if (diff) item2 = item.key[0];
-			else item2 = item.key;
-    			if(newObj[item2] === undefined) newObj[item2] = 0;
-   			newObj[item2] += item.value;
-		}
-		var result = {};
-		result.rows = [];
-		for(i in newObj) result.rows.push({'key':i,'value':newObj[i]});
+	//sort
+	var newObj = sort(data)[1];
 
-		//sort table
-		result.rows.sort(function(a,b){return b.value-a.value})
-		//make new data for d3 chart
-		var chart = {
-			     "name": "hostname",
-			     "children": [
-			     ]};
-		var nOfEntry = 0;
-		var valueOfEntry = 0;
-		for (i in newObj) {
-			nOfEntry += 1;
-			valueOfEntry += newObj[i];
-		}
-		var minInclude = valueOfEntry / nOfEntry * 0.5;
-		for (i in newObj) {
-			if (newObj[i] >= minInclude) chart.children.push({'name':i,'size':newObj[i]});
-		}
+	//make new data for d3 chart
+	var chart = {
+		"name": "hostname",
+		"children": [
+	]};
+	var nOfEntry = 0;
+	var valueOfEntry = 0;
+	for (i in newObj) {
+		nOfEntry += 1;
+		valueOfEntry += newObj[i];
+	}
+	var minInclude = valueOfEntry / nOfEntry * 0.5;
+	for (i in newObj) {
+		if (newObj[i] >= minInclude) chart.children.push({'name':i,'size':newObj[i]});
+	}
 
-		//draw chart
-		var diameter = $(struct).width(),
-		format = d3.format(",d"),
-    	color = d3.scale.category20c();
-		var bubble = d3.layout.pack()
-    			.sort(null)
-    			.size([diameter,diameter])
-				.padding(10);
-		var svg = d3.select(struct).append("svg")
-			.attr("width", diameter)
-			.attr("height", diameter)
-			.attr("class", "bubble");
+	//draw chart
+	var diameter = $(struct).width(),
+	format = d3.format(",d"),
+	color = d3.scale.category20c();
 
-		var node = svg.selectAll(".node")
-			.data(bubble.nodes(classes(chart))
-			.filter(function(d) { return !d.children; }))
-			.enter().append("g")
-			.attr("class", "node")
-			.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	var bubble = d3.layout.pack()
+			.sort(null)
+			.size([diameter,diameter])
+			.padding(10);
 
-		node.append("title")
-			.text(function(d) { return d.className + ": " + format(d.value); });
+	var svg = d3.select(struct).append("svg")
+		.attr("width", diameter)
+		.attr("height", diameter)
+		.attr("class", "bubble");
 
-		node.append("circle")
-			.attr("r", function(d) { return d.r; })
-			.style("fill", function(d) { return color(d.packageName); });
+	var node = svg.selectAll(".node")
+		.data(bubble.nodes(classes(chart))
+		.filter(function(d) { return !d.children; }))
+		.enter().append("g")
+		.attr("class", "node")
+		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-		node.append("text")
-			.attr("dy", ".5em")
-			.style("text-anchor", "middle")
-			.text(function(d) { return d.className.substring(0, d.r / 3); });
+	node.append("title")
+		.text(function(d) { return d.className + ": " + format(d.value); });
 
-		// Returns a flattened hierarchy containing all leaf nodes under the root.
-		function classes(chart) {
-			var classes = [];
+	node.append("circle")
+		.attr("r", function(d) { return d.r; })
+		.style("fill", function(d) { return color(d.packageName); });
 
-			function recurse(name, node) {
-				if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-				else classes.push({packageName: name, className: node.name, value: node.size});
-			}
+	node.append("text")
+		.attr("dy", ".5em")
+		.style("text-anchor", "middle")
+		.text(function(d) { return d.className.substring(0, d.r / 3); });
 
-			recurse(null, chart);
-			return {children: classes};
+	// Returns a flattened hierarchy containing all leaf nodes under the root.
+	function classes(chart) {
+		var classes = [];
+
+		function recurse(name, node) {
+			if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
+			else classes.push({packageName: name, className: node.name, value: node.size});
 		}
 
-		d3.select(self.frameElement).style("height", diameter + "px");
+		recurse(null, chart);
+		return {children: classes};
+	}
+
+	d3.select(self.frameElement).style("height", diameter + "px");
 }
 
 function drawTable(struct, data) {
+	//sort
+	var result = sort(data)[0];
+
 	var table = $(struct);
 	table.empty();
-	var valueCount = 0;
-	data.rows.sort(function(a,b){return b.value-a.value})
-	$.each(data.rows, function(index, entry){
-		valueCount = valueCount + entry.value
+	var sum = 0;
+	$.each(result.rows, function(index, entry){
+		sum = sum + entry.value
 		var row = $("<tr><td>"+entry.key+"</td><td>"+entry.value+"</td></tr>");
 		table.append(row);
 	})
-	var row = $("<tr style=\"border-top: 1px solid #000;\"><td>Gesamt</td><td>"+valueCount+"</td></tr>");
+	var row = $("<tr style=\"border-top: 0.063em solid #000;\"><td>Gesamt</td><td>"+sum+"</td></tr>");
 	table.append(row);
 }
 
@@ -107,17 +117,17 @@ function stats() {
 			//is an extended url
 			var isDomain = 0;
 			$.ajax({
-    			url: "./json/domains.json",
-    			async: false,
-    			dataType: 'json',
-    			success: function(domains) {
-    				$.each(domains.children, function(_index, domainsEntry) {
+				url: "./json/domains.json",
+				async: false,
+				dataType: 'json',
+				success: function(domains) {
+					$.each(domains.children, function(_index, domainsEntry) {
 						for (i in domainsEntry) {
 							if (hostname == domainsEntry[i]) isDomain = 1;
 						}
 					});
-    			}
-    		});
+				}
+			});
 			//if not ip show only hostname
 			if (!/[0-9]/.test(hn[0].charAt(0)) & !isDomain) entry.key[0] = hostname;
 			else if (isDomain) entry.key[0] = hn[2] + "." + hostname;
